d68a9e4340a8930b946d3da4c175cdbba108a1d9
fionicss <118982867+Fionicss@users.noreply.github.com>
2024-05-03 16:22:40 +0800

    Project initialization.

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body',
+        })
+    ]
+};
